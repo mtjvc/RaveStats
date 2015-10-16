ba729ddfe05cc6979b71bb2dd21c7216607b726d
Gal Matijevic <gal@batman.local>
2015-10-16 11:29:00 +0200

    merge

--- color_palettes.py
+++ color_palettes.py
@@ -4,8 +4,8 @@ from gma.colormaps import viridis
 
 
 N = 25
-for k, i in enumerate(np.linspace(0.0, 1.0, N)):
-    rgb = np.array(np.array(viridis(i)[:3]) * 256, dtype=int)
+for k, i in enumerate(np.linspace(0.0, 0.95, N)):
+    rgb = np.array(np.array(pl.cm.gray(i)[:3]) * 256, dtype=int)
 
-    print ".Viridis .q%d-%d{fill:rgb(%d,%d,%d)}" % (k, N, rgb[0],
+    print ".Gray .q%d-%d{fill:rgb(%d,%d,%d)}" % (k, N, rgb[0],
                                                     rgb[1], rgb[2])
